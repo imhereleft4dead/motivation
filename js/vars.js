4e58bdcff3d677ad3588f958c779135ab2a458f6
imhereleft4dead <32651207+imhereleft4dead@users.noreply.github.com>
2017-10-09 12:08:39 -0700

    Update vars.js

--- js/vars.js
+++ js/vars.js
@@ -2,4 +2,4 @@
 
 var sleepingTimeHours = 12, // The hour you sleep. In my case I sleep at 12:00pm
 sleepingTimeMinutes = 00, // The minutes remaining for the time you sleep.
-dateOfBirth = 'August 31 1997';
+dateOfBirth = 'April 27 2001';
